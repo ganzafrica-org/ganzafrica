5a50ed827a6993efaa20cc7e6ce0bb2ac7989683
Baho Kemana Joselyto Charite <88556861+Josy-coder@users.noreply.github.com>
2025-03-28 14:29:21 +0200

    Feature/homepage (#17)
    * [fix] updated hero on homepage
    
    * [fix] updated fellowssection on homepage
    
    * [fix] header, hero section homepage, etc...
    
    * [fix] switched with what makes ganzafrica unique

--- apps/web/app/[locale]/page.tsx
+++ apps/web/app/[locale]/page.tsx
@@ -1,5 +1,4 @@
 import { getDictionary } from "@/lib/get-dictionary";
-import HomeHero from "@/components/sections/homepage/home-hero";
 import FellowsSection from "@/components/sections/homepage/fellows-section";
 import WhyGanzAfricaSection from "@/components/sections/homepage/why-ganzafrica-section";
 import FlagshipProgramsSection from "@/components/sections/homepage/flagship-programs-section";
@@ -9,6 +8,8 @@ import PartnersSection from "@/components/sections/homepage/partners-section";
 import { Metadata } from "next";
 import { generateMetadata as baseGenerateMetadata } from "@/lib/metadata";
 import LatestNewsSection from "@/components/sections/homepage/latest-news-section";
+import NewsletterSection from "@/components/sections/newsletter-section";
+import GanzAfricaUniqueSection from "@/components/sections/homepage/ganzafrica-unique-section";
 
 // Generate metadata for SEO
 export async function generateMetadata({
@@ -40,14 +41,16 @@ export default async function HomePage({
 
     return (
         <main>
-            <HomeHero locale={locale} dict={dict} />
+
             <FellowsSection locale={locale} dict={dict} />
             <WhyGanzAfricaSection locale={locale} dict={dict} />
+            <GanzAfricaUniqueSection locale={locale} dict={dict} />
             <FlagshipProgramsSection locale={locale} dict={dict} />
             <ProjectsSection locale={locale} dict={dict} />
             <PartnersSection locale={locale} dict={dict} />
             <TestimonialsSection locale={locale} dict={dict} />
             <LatestNewsSection locale={locale} dict={dict} />
+            <NewsletterSection locale={locale} dict={dict} />
         </main>
     );
 }
