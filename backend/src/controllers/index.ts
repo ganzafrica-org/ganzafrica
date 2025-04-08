60e9e7815c185580163c36912655a944db707b9d
Baho Kemana Joselyto Charite <88556861+Josy-coder@users.noreply.github.com>
2025-04-08 16:19:38 +0200

    Feature/homepage (#23)
    * [fix] updated hero on homepage
    
    * [fix] updated fellowssection on homepage
    
    * [fix] header, hero section homepage, etc...
    
    * [fix] switched with what makes ganzafrica unique
    
    * [fix] modified the food system page to match design
    
    * [fix] modified the climate adaptation page to match design
    
    * [chore] setup backend
    
    * [chore] setup backend 2
    
    * [chore] setup backend 3
    
    * [chore] setup backend 3
    
    * [chore] setup backend 3

---
+++ backend/src/controllers/index.ts
@@ -0,0 +1,9 @@
+import * as authController from './auth.controller';
+import * as userController from './user.controller';
+import * as projectController from './project.controller';
+
+export {
+    authController,
+    userController,
+    projectController,
+};
---
+++ backend/src/services/index.ts
@@ -0,0 +1,11 @@
+import * as authService from './auth.service';
+import * as userService from './user.service';
+import * as projectService from './project.service';
+import * as emailService from './email.service';
+
+export {
+    authService,
+    userService,
+    projectService,
+    emailService,
+};
