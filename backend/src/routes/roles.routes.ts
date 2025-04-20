d15170d5cd20e415f0683c1c9029120a5d58ea5a
uwasejeannine <jeannineuwase@gmail.com>
2025-04-20 13:01:04 +0200

    Merge branch 'dev' of https://github.com/ganzafrica-org/ganzafrica into feature/dev-changes

--- backend/src/routes/roles.routes.ts
+++ backend/src/routes/roles.routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { roleController } from "../controllers/roles";
 import { validate, authenticate, authorize } from "../middlewares";
 import { roleValidation } from "../validations/roles.validation";
+import { constants } from "../config";
 
 const router: Router = Router();
 
@@ -15,48 +16,56 @@ const router: Router = Router();
 // All routes require authentication
 router.use(authenticate);
 
-// Role routes
+// Role management routes (Admin only)
 router.post(
   "/",
+  authorize([constants.ROLES.ADMIN]),
   validate(roleValidation.createRoleSchema),
   roleController.createRole,
 );
 
-router.get("/", roleController.listRoles);
+// Get roles list - accessible by admin and staff members
+router.get("/", authorize([constants.ROLES.ADMIN, constants.ROLES.STAFF]), roleController.listRoles);
 
 router.get(
   "/:id",
+  authorize([constants.ROLES.ADMIN, constants.ROLES.STAFF]),
   validate(roleValidation.getRoleSchema),
   roleController.getRoleById,
 );
 
 router.put(
   "/:id",
+  authorize([constants.ROLES.ADMIN]),
   validate(roleValidation.updateRoleSchema),
   roleController.updateRole,
 );
 
 router.delete(
   "/:id",
+  authorize([constants.ROLES.ADMIN]),
   validate(roleValidation.deleteRoleSchema),
   roleController.deleteRole,
 );
 
-// User role management routes
+// User role management routes (Admin only)
 router.get(
   "/users/:userId",
+  authorize([constants.ROLES.ADMIN]),
   validate(roleValidation.getUserRolesSchema),
   roleController.getUserRoles,
 );
 
 router.post(
   "/users/:userId/assign/:roleId",
+  authorize([constants.ROLES.ADMIN]),
   validate(roleValidation.assignRoleSchema),
   roleController.assignRoleToUser,
 );
 
 router.delete(
   "/users/:userId/remove/:roleId",
+  authorize([constants.ROLES.ADMIN]),
   validate(roleValidation.removeRoleSchema),
   roleController.removeRoleFromUser,
 );
