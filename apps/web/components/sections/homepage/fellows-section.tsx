9687d66b1c0ee459d9a650aba9f7a78efc05b63f
Baho Kemana Joselyto Charite <88556861+Josy-coder@users.noreply.github.com>
2025-04-30 13:18:56 +0200

    [fix] issues on home page and other pages

--- apps/web/components/sections/homepage/fellows-section.tsx
+++ apps/web/components/sections/homepage/fellows-section.tsx
@@ -37,101 +37,87 @@ export default function FellowsSection({ locale, dict }: FellowsSectionProps) {
   }, []);
 
   return (
-    <section
-      ref={sectionRef}
-      className="pt-16 md:pt-24 bg-white relative overflow-hidden"
-    >
-      <div className="container mx-auto px-4">
-      <div className="text-center mb-12">
-  <DecoratedHeading
-    firstText={dict.home?.fellow_section?.heading_first || "Empower Youth to"}
-    secondText={dict.home?.fellow_section?.heading_second || "Transform Africa's Food Systems"}
-    firstTextColor="text-black"
-    secondTextColor="text-primary-green"
-    borderColor="border-primary-green"
-    cornerColor="bg-primary-orange"
-    className="mx-auto"
-  />
-</div>
-      </div>
-
-      <div className="relative container mx-auto px-4">
-        {/* L-shaped background div */}
-        <div className="absolute left-0 right-0 top-0 bottom-0">
-          <svg width="100%" height="100%" className="absolute inset-0">
-            <path
-              d="M 0,0
-                               L 70%,0
-                               L 70%,35%
-                               L 100%,35%
-                               L 100%,100%
-                               L 0,100%
-                               Z"
-              fill="#FFF9E5"
+      <section
+          ref={sectionRef}
+          className="pt-16 md:pt-24 bg-white relative overflow-hidden"
+      >
+        <div className="container mx-auto px-4">
+          <div className="text-center mb-12">
+            <DecoratedHeading
+                firstText={dict.home?.fellow_section?.heading_first || "Empower Youth to"}
+                secondText={dict.home?.fellow_section?.heading_second || "Transform Africa's Food Systems"}
+                firstTextColor="text-black"
+                secondTextColor="text-primary-green"
+                borderColor="border-primary-green"
+                cornerColor="bg-primary-orange"
+                className="mx-auto"
             />
-          </svg>
+          </div>
         </div>
 
-        <div className="relative z-10 flex flex-col lg:flex-row items-start gap-8">
-          {/* Content section */}
-          <div ref={contentRef} className="flex-1 lg:pl-8 pt-8">
-            <p className="text-gray-800 text-lg mb-8">
-              {dict.home?.fellow_section?.description ||
-                `GanzAfrica empowers Africa's youth with the skills, knowledge, and
-                                opportunities to drive sustainable food systems transformation.
-                                Through training, mentorship, and work placements, we equip young
-                                leaders to tackle challenges in agriculture, environmental
-                                stewardship, and land management. Our holistic approach integrates
-                                data literacy, evidence-based decision-making, and leadership
-                                development, bridging the gap between education and employment.
-                                By fostering expertise and professional networks, GanzAfrica
-                                prepares youth to build resilient communities and contribute to a
-                                thriving Africa.`}
-            </p>
-            <div className="flex flex-wrap md:flex-nowrap  gap-4 my-8">
-  <div className="flex items-center space-x-8">
-    <div className="w-3 h-3 rounded-full bg-[#FFB800]"></div>
-    <p className="text-gray-800 font-medium">
-      {dict.home?.fellow_section?.Training || "Training"}
-    </p>
-  </div>
-  <div className="flex items-center space-x-8">
-    <div className="w-3 h-3 rounded-full bg-[#FFB800]"></div>
-    <p className="text-gray-800 font-medium">
-      {dict.home?.fellow_section?.career_placement || "Career Placement"}
-    </p>
-  </div>
-  <div className="flex items-center space-x-8">
-    <div className="w-3 h-3 rounded-full bg-[#FFB800]"></div>
-    <p className="text-gray-800 font-medium">
-      {dict.home?.fellow_section?.Mentorship || "Mentorship"}
-    </p>
-  </div>
-  <div className="flex items-center space-x-8">
-    <div className="w-3 h-3 rounded-full bg-[#FFB800]"></div>
-    <p className="text-gray-800 font-medium">
-      {dict.home?.fellow_section?.data_solutions || "Data Driven Impact"}
-    </p>
-  </div>
-</div>
-          </div>
-          {/* Image section positioned to overlap the step */}
-          <div className="relative flex-1 w-full max-w-lg mx-auto lg:mx-0 lg:-mb-16">
-            <div className="relative rounded-2xl overflow-hidden shadow-xl w-full h-[300px] md:h-[400px]">
-              <Image
-                src={
-                  dict.home?.fellow_section?.image_src ||
-                  "/images/ganzafrica-fellows.jpg"
-                }
-                alt={dict.about?.fellows_photo_alt || "GanzAfrica fellows"}
-                fill
-                priority
-                className="object-cover"
-              />
+        <div className="relative container mx-auto px-4">
+
+          <div className="relative z-10 flex flex-col lg:flex-row items-start gap-8">
+            {/* Content section - reduced width */}
+            <div ref={contentRef} className="w-full lg:basis-[45%] pt-8">
+              <p className="text-gray-800 text-lg mb-8">
+                {dict.home?.fellow_section?.description ||
+                    `GanzAfrica empowers Africa's youth with the skills, knowledge, and
+                opportunities to drive sustainable food systems transformation.
+                Through training, mentorship, and work placements, we equip young
+                leaders to tackle challenges in agriculture, environmental
+                stewardship, and land management. Our holistic approach integrates
+                data literacy, evidence-based decision-making, and leadership
+                development, bridging the gap between education and employment.
+                By fostering expertise and professional networks, GanzAfrica
+                prepares youth to build resilient communities and contribute to a
+                thriving Africa.`}
+              </p>
+              <div className="flex flex-wrap md:flex-nowrap gap-4 my-8">
+                <div className="flex items-center space-x-8">
+                  <div className="w-3 h-3 rounded-full bg-[#FFB800]"></div>
+                  <p className="text-gray-800 font-medium">
+                    {dict.home?.fellow_section?.Training || "Training"}
+                  </p>
+                </div>
+                <div className="flex items-center space-x-8">
+                  <div className="w-3 h-3 rounded-full bg-[#FFB800]"></div>
+                  <p className="text-gray-800 font-medium">
+                    {dict.home?.fellow_section?.career_placement || "Career Placement"}
+                  </p>
+                </div>
+                <div className="flex items-center space-x-8">
+                  <div className="w-3 h-3 rounded-full bg-[#FFB800]"></div>
+                  <p className="text-gray-800 font-medium">
+                    {dict.home?.fellow_section?.Mentorship || "Mentorship"}
+                  </p>
+                </div>
+                <div className="flex items-center space-x-8">
+                  <div className="w-3 h-3 rounded-full bg-[#FFB800]"></div>
+                  <p className="text-gray-800 font-medium">
+                    {dict.home?.fellow_section?.data_solutions || "Data Driven Impact"}
+                  </p>
+                </div>
+              </div>
+            </div>
+            {/* Image section - increased width */}
+            <div className="relative w-full lg:basis-[55%]  mx-auto lg:mx-0 lg:-mb-16">
+              <div className="relative rounded-2xl overflow-hidden shadow-md w-full h-[300px] md:h-[400px]">
+                <Image
+                    src={
+                        dict.home?.fellow_section?.image_src ||
+                        "/images/ganzafrica-fellows.jpg"
+                    }
+                    alt={dict.about?.fellows_photo_alt || "GanzAfrica fellows"}
+                    fill
+                    priority
+                    className="object-cover"
+                    style={{ objectPosition: '50% 30%' }}
+                />
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
   );
 }
